a445b2b496186549103724f888b6559a1a23475e
VyDee <vy.duong@cuw.edu>
2018-07-03 00:33:51 -0500

    7/3/18
    update

---
+++ Assets/Unity UI Scripting/Scripts/Music/ChangeMusicVolume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMusicVolume : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource myMusic;
+
+    private void Start()
+    {
+        Volume.value = 0.5f;
+    }
+    
+    void Update ()
+    {
+        myMusic.volume = Volume.value;
+
+	}
+}
+
+/*void MusicAdjust
+ * if the master volume = 0 
+        music volume = 0
+   else the master != 0
+        music volume = Mathf.Max (0, master.value)
+        scene volume = music volume*/
+
